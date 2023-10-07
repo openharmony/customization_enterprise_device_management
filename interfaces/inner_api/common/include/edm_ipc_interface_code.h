60f7d5c0261dea0c9e8482fc894ee7ea7e064fd8
ning <214848454@qq.com>
2023-10-07 09:55:55 +0800

    安全接口开发
    Signed-off-by: ning <zhangning134@huawei.com>

--- interfaces/inner_api/common/include/edm_ipc_interface_code.h
+++ interfaces/inner_api/common/include/edm_ipc_interface_code.h
@@ -61,6 +61,7 @@ enum EdmInterfaceCode : uint32_t {
     USB_READ_ONLY = 1026,
     INSTALL_CERTIFICATE = 1027,
     INSTALL = 1028,
+    GET_SECURITY_PATCH_TAG = 1029,
     POLICY_CODE_END = 3000,
 };
 } // namespace EDM
