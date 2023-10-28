e61febce20fc92a0df11dac4d1ad85a0de345eb5
yaobaohua <yaobaohua1@huawei.com>
2023-10-28 09:46:59 +0800

    cfi option
    Signed-off-by: yaobaohua <yaobaohua1@huawei.com>

--- services/edm_plugin/src/set_wifi_profile_plugin.cpp
+++ services/edm_plugin/src/set_wifi_profile_plugin.cpp
@@ -36,7 +36,7 @@ void SetWifiProfilePlugin::InitPlugin(
     ptr->SetOnHandlePolicyListener(&SetWifiProfilePlugin::OnSetPolicy, FuncOperateType::SET);
 }
 
-ErrCode SetWifiProfilePlugin::OnSetPolicy(Wifi::WifiDeviceConfig &config)
+ErrCode SetWifiProfilePlugin::OnSetPolicy(Wifi::WifiDeviceConfig &config) __attribute__((no_sanitize("cfi")))
 {
     EDMLOGD("SetWifiProfilePlugin OnSetPolicy");
     ErrCode ret = Wifi::WifiDevice::GetInstance(WIFI_DEVICE_ABILITY_ID)->ConnectToDevice(config);
