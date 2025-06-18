4e9ca98c295e38f99ccd224af7ba79305d284468
hanKF <hanhu3@h-partners.com>
2025-06-18 10:09:12 +0000

    local
    Signed-off-by: hanKF <hanhu3@h-partners.com>

---
+++ services/edm/src/query_policy/get_install_local_enterprise_app_enabled_queery.cpp
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+#include "get_install_local_enterprise_app_enabled_query.h"
+ 
+#include "edm_constants.h"
+#include "parameters.h"
+ 
+namespace OHOS {
+namespace EDM {
+std::string GetInstallLocalEnterpriceAppEnabledQuery::GetPolicyName()
+{
+    return PolicyName::POLICY_SET_INSTALL_LOCAL_ENTERPRISE_APP_ENABLED;
+}
+ 
+std::string GetInstallLocalEnterpriceAppEnabledQuery::GetPermission(IPlugin::PermissionType, const std::string &permissionTag)
+{
+    return EdmPermission::PERMISSION_ENTERPRISE_MANAGE_SYSTEM;
+}
+ 
+ErrCode GetInstallLocalEnterpriceAppEnabledQuery::QueryPolicy(std::string &policyData, MessageParcel &data, MessageParcel &reply,
+    int32_t userId)
+{
+    return GetBoolPolicy(policyData, reply);
+}
+} // namespace EDM
+} // namespace OHOS
