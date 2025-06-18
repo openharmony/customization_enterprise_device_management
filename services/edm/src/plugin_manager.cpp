ced6ef85d1328d85ceb19266ad76afb543841516
hanKF <hanhu3@h-partners.com>
2025-06-18 10:09:22 +0000

    local
    Signed-off-by: hanKF <hanhu3@h-partners.com>

--- services/edm/src/plugin_manager.cpp
+++ services/edm/src/plugin_manager.cpp
@@ -62,7 +62,8 @@ std::vector<uint32_t> PluginManager::deviceCoreSoCodes_ = {
     EdmInterfaceCode::DISALLOW_POWER_LONG_PRESS, EdmInterfaceCode::ALLOWED_KIOSK_APPS,
     EdmInterfaceCode::SET_KIOSK_FEATURE, EdmInterfaceCode::DISABLE_SET_BIOMETRICS_AND_SCREENLOCK,
     EdmInterfaceCode::DISABLE_SET_DEVICE_NAME, EdmInterfaceCode::SET_AUTO_UNLOCK_AFTER_REBOOT,
-    EdmInterfaceCode::DISABLE_MTP_CLIENT, EdmInterfaceCode::DISABLE_MTP_SERVER
+    EdmInterfaceCode::DISABLE_MTP_CLIENT, EdmInterfaceCode::DISABLE_MTP_SERVER,
+    EdmInterfaceCode::SET_INSTALL_LOCAL_ENTERPRISE_APP_ENABLED,
 };
 
 std::vector<uint32_t> PluginManager::communicationSoCodes_ = {
---
+++ services/edm_plugin/src/set_install_local_enterprise_app_enabled_plugin.cpp
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+#include "set_install_local_enterprise_app_enabled_plugin.h"
+ 
+#include "bool_serializer.h"
+#include "edm_ipc_interface_code.h"
+#include "edm_constants.h"
+#include "iplugin_manager.h"
+ 
+namespace OHOS {
+namespace EDM {
+const bool REGISTER_RESULT = IPluginManager::GetInstance()->AddPlugin(SetInstallLocalEnterpriseAppEnabledPlugin::GetPlugin());
+ 
+void SetInstallLocalEnterpriseAppEnabledPlugin::InitPlugin(
+    std::shared_ptr<IPluginTemplate<SetInstallLocalEnterpriseAppEnabledPlugin, bool>> ptr)
+{
+    EDMLOGI("SetInstallLocalEnterpriseAppEnabledPlugin InitPlugin...");
+    ptr->InitAttribute(EdmInterfaceCode::SET_INSTALL_LOCAL_ENTERPRISE_APP_ENABLED, PolicyName::POLICY_SET_INSTALL_LOCAL_ENTERPRISE_APP_ENABLED,
+        EdmPermission::PERMISSION_ENTERPRISE_MANAGE_SYSTEM, IPlugin::PermissionType::SUPER_DEVICE_ADMIN);
+    ptr->SetSerializer(BoolSerializer::GetInstance());
+    ptr->SetOnHandlePolicyListener(&SetInstallLocalEnterpriseAppEnabledPlugin::OnSetPolicy, FuncOperateType::SET);
+    ptr->SetOnAdminRemoveListener(&SetInstallLocalEnterpriseAppEnabledPlugin::OnAdminRemove);
+    persistParam_ = "persist.edm.is_local_install_enable";
+}
+} // namespace EDM
+} // namespace OHOS
