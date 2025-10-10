afe848208ace70011f947363456541b386388d34
guolongfei <termBG@petalmail.com>
2025-10-10 22:14:49 +0800

    补充UT测试用例
    Signed-off-by: guolongfei <termBG@petalmail.com>

---
+++ test/unittest/services/edm_plugin/src/disallow_unmute_device_plugin_test.cpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+
+#include "disallow_unmute_device_plugin.h"
+#include "edm_ipc_interface_code.h"
+#include "utils.h"
+
+using namespace testing::ext;
+using namespace testing;
+
+namespace OHOS {
+namespace EDM {
+namespace TEST {
+class DisallowUnmuteDevicePluginTest : public testing::Test {
+protected:
+    static void SetUpTestSuite(void);
+
+    static void TearDownTestSuite(void);
+};
+
+void DisallowUnmuteDevicePluginTest::SetUpTestSuite(void)
+{
+    Utils::SetEdmInitialEnv();
+}
+
+void DisallowUnmuteDevicePluginTest::TearDownTestSuite(void)
+{
+    Utils::ResetTokenTypeAndUid();
+    ASSERT_TRUE(Utils::IsOriginalUTEnv());
+    std::cout << "now ut process is orignal ut env : " << Utils::IsOriginalUTEnv() << std::endl;
+}
+
+/**
+ * @tc.name: TestOnSetPolicy
+ * @tc.desc: Test DisallowUnmuteDevicePluginTest::OnSetPolicy function.
+ * @tc.type: FUNC
+ */
+HWTEST_F(DisallowUnmuteDevicePluginTest, TestOnSetPolicy, TestSize.Level1)
+{
+    MessageParcel data;
+    MessageParcel reply;
+    data.WriteBool(false);
+    auto plugin = DisallowUnmuteDevicePlugin::GetPlugin();
+    HandlePolicyData handlePolicyData{"false", "", false};
+    std::uint32_t funcCode = POLICY_FUNC_CODE((std::uint32_t)FuncOperateType::SET,
+        EdmInterfaceCode::DISALLOW_UNMUTE_DEVICE);
+    ErrCode ret = plugin->OnHandlePolicy(funcCode, data, reply, handlePolicyData, DEFAULT_USER_ID);
+    ASSERT_TRUE(ret == ERR_OK);
+    ASSERT_TRUE(handlePolicyData.isChanged);
+}
+} // namespace TEST
+} // namespace EDM
+} // namespace OHOS
