0d24dcf0eb5c79f4244357bd80cad5f3e425dea9
ZhuAgile <zhumingyong1@h-partners.com>
2025-10-12 17:42:52 +0800

    支持外部扩展程序管控策略
    Signed-off-by: ZhuAgile <zhumingyong1@h-partners.com>

--- services/edm/src/plugin_manager.cpp
+++ services/edm/src/plugin_manager.cpp
@@ -102,7 +102,7 @@ std::vector<uint32_t> PluginManager::sysServiceSoCodes_ = {
     EdmInterfaceCode::CLIPBOARD_POLICY, EdmInterfaceCode::MANAGE_KEEP_ALIVE_APPS,
     EdmInterfaceCode::CLEAR_UP_APPLICATION_DATA, EdmInterfaceCode::DISALLOWED_NOTIFICATION,
     EdmInterfaceCode::PERMISSION_MANAGED_STATE, EdmInterfaceCode::ALLOW_RUNNING_BUNDLES,
-    EdmInterfaceCode::DISABLE_RUNNING_BINARY_APP,
+    EdmInterfaceCode::DISABLE_RUNNING_BINARY_APP
 };
 
 std::vector<uint32_t> PluginManager::needExtraSoCodes_ = {
